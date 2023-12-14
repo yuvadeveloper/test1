d00ebd75b4b321d8f4b1fc8d68316a2a682ec08b
yuvadeveloper <135313371+yuvadeveloper@users.noreply.github.com>
2023-12-14 13:41:49 +0530

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+}
---
+++ android/app/build/generated/source/imgly/debug/_IMGLYInternalConfig_.kt
@@ -0,0 +1,12 @@
+@file:Suppress("MayBeConstant") // Const not possible because this class overrides a header only file
+package ly.img.android.sdk
+
+import ly.img.android.pesdk.annotations.FinalEventCollection
+
+@Suppress("ClassName")
+@FinalEventCollection
+object _IMGLYInternalConfig_ {
+    @JvmField val versionName: String = "10.4.1"
+    @JvmField val pesdkLicensePath: String = ""
+    @JvmField val vesdkLicensePath: String = ""
+}
